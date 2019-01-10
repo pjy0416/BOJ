f4e41163473bb9c1b9e4aa5e1ac8c30751539afe
dimpl@JinYoung <dimple0416@naver.com>
2019-01-10 18:28:11 +0900

    update : solution of Test 8611

--- BOJ/src/Test_8611.java
+++ BOJ/src/Test_8611.java
@@ -1,58 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
-public class Test_8611 {				// �Է� ������ 10^1000 �����ε� �ٿ��� ó���ϴ°� ����..
-										// double���� �Ҽ��� ����
-	static final short max =10;
-	static void getResult(Double num) {
+public class Test_8611 {
+	static void getResult(BigInteger num) {
+		String numStr = "" + num;
 		int cnt =0;
-		
-		for(int sys=2; sys<max; sys++) {
+
+		for(int sys=2; sys<10; sys++) {
 			String str = convertSys(sys,num);
 			if(isPalin(str)) {
 				cnt ++;
 				System.out.println(sys + " " + str);
 			}
 		}
+
+		if(isPalin(numStr)) {
+			System.out.println(10 + " " + numStr);
+			cnt++;
+		}
+
 		if(cnt==0) {
 			System.out.println("NIE");
 		}
 	}
-	
+
 	static boolean isPalin(String str) {
 		int len = str.length();
 		int end = str.length()-1;
 		int center = len/2;
-		
+
 		for(int start =0; start <center; start++) {
 			if(str.charAt(start) != str.charAt(end - start)) {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
-	static String convertSys(int sys, Double num) {
+
+	static String convertSys(int sys, BigInteger num) {
 		String result ="";
-		double temp = num;
-		while(temp > 0.0) {
-			System.out.println(temp);
-			result = String.valueOf(temp%sys).concat(result);
-			temp /= sys;
+
+		BigInteger temp = num;
+		BigInteger param = new BigInteger("0");
+		BigInteger bigSys = BigInteger.valueOf(sys);
+
+		while(temp.compareTo(param) == 1) {
+			result = temp.mod(bigSys) + result;
+			temp = temp.divide(bigSys);
 		}
 		return result;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		Double num = Double.parseDouble(bf.readLine());
-		
-	
+		BigInteger num = new BigInteger(bf.readLine());
+
 		getResult(num);
-		
 		bf.close();
 	}
 
