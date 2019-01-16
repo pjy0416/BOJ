c1e1b4430982ffcefb9090b2a08825e3ca29936d
dimpl@JinYoung <dimple0416@naver.com>
2019-01-16 21:37:35 +0900

    fix : Test 11658 but failed (구간 합 구하기 3)(over time)

--- BOJ/src/test_11658.java
+++ BOJ/src/test_11658.java
@@ -1,93 +1,72 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class test_11658 {			// time over code....	아마 String Array를 int Array로 parse하는 부분에서 시간이 걸리는듯....
+public class test_11658 {			// time over code....	add 부분에서 시간이 오래걸리는듯... 
 	
-//	static int[] parseIntArr(String[] strArr, int[] intArr) {
-//				
-//		intArr = Arrays.stream(strArr).mapToInt(Integer::parseInt).toArray();
-//		
-//		return intArr;
-//	}
-	
-	static int[][] initArr(int arrLen, Scanner scan) {
-		int[][] result = new int[arrLen][];
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	static String[][] initArr(int arrLen) throws IOException{
+		String[][] result = new String[arrLen][];
 		
 		for(int i=0; i<arrLen; i++) {
-			String inputStr = scan.nextLine();
-			String[] inputArr = inputStr.split(" ");
-
-			int idx =0;
-			int len = inputArr.length;
-			result[i] = new int[len];
-			
-			for(String str : inputArr) {
-				result[i][idx++] = Integer.parseInt(str);
-			}
-			
-//			result[i] = parseIntArr(inputArr, result[i]);
+			String inputStr = br.readLine();
+			result[i] = inputStr.split(" ");
 		}
 		
 		return result;
 	}
-	
-	static void arrayOperator(int[][] arr, int[][] queries, int arrSize, int querySize) {
+
+	static void runQuery(String[][] arr, int querySize) throws IOException {
+
 		for(int i=0; i<querySize; i++) {
-			int[] query = queries[i];
+			String str = br.readLine();
+			String[] query = str.split(" ");
 			int len = query.length;
-			
-			switch (len) {
-				case 4 :
-					arr = arrayModifier(arr, query, arrSize, querySize);					
-					break;
-				case 5 :
-					System.out.println(addSection(arr, query, arrSize, querySize));
-					break;
-				default : 
-					break;
+
+			if(len == 4) {
+				arrayModifier(arr, query);
+			} else {
+				addSection(arr, query);
 			}
 		}
 	}
 	
-	static int[][] arrayModifier(int[][] arr, int[] query, int arrSize, int querySize) {	// arr 수정
-		int x = query[1]-1;
-		int y = query[2]-1;
-		int value = query[3];
-		
-		arr[x][y] = value;
+	static String[][] arrayModifier(String[][] arr, String[] query) {	// arr 수정
+		int x = Integer.parseInt(query[1])-1;
+		int y = Integer.parseInt(query[2])-1;
+
+		arr[x][y] = query[3];
 		
 		return arr;
 	}
 	
-	static int addSection(int[][] arr, int[] query, int arrSize, int querySize) {		// arr 연산
+	static void addSection(String[][] arr, String[] query) {		// arr 연산
+		int startX = Integer.parseInt(query[1])-1;
+		int startY = Integer.parseInt(query[2])-1;
+		int endX = Integer.parseInt(query[3])-1;
+		int endY = Integer.parseInt(query[4])-1;
+
 		int result =0;
-		
-		int startX = query[1]-1;
-		int startY = query[2]-1;
-		int endX = query[3]-1;
-		int endY = query[4]-1;
-		
 		for(int x=startX; x<=endX; x++) {
 			for(int y=startY; y<=endY; y++) {
-				result += arr[x][y];
+				result += Integer.parseInt(arr[x][y]);
 			}
 		}
-		
-		return result;
+
+		System.out.println(result);
 	}
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+
+		String sizes = br.readLine();
+		int arrSize = Integer.parseInt(sizes.split(" ")[0]);
+		int querySize = Integer.parseInt(sizes.split(" ")[1]);
 		
-		int arrSize = scan.nextInt();
-		int querySize = scan.nextInt();
-		
-		scan.nextLine(); //개행문자 제거
-		
-		int[][] array = initArr(arrSize, scan);
-		int[][] queries = initArr(querySize, scan);
-		
-		arrayOperator(array, queries, arrSize, querySize);
-		
-		scan.close();
+		String[][] array = initArr(arrSize);
+
+		runQuery(array, querySize);
+
+		br.close();
 	}
 }
