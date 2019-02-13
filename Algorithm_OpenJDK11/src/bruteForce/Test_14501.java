8ba28452ef76ae88394b9f098b21c52e529371ce
dimpl@JinYoung <dimple0416@naver.com>
2019-02-13 14:58:21 +0900

    Worng code (Brute Force 퇴사문제)(예시 테스트케이스는 통과)

--- Algorithm_OpenJDK11/src/bruteForce/Test_14501.java
+++ Algorithm_OpenJDK11/src/bruteForce/Test_14501.java
@@ -7,47 +7,65 @@ import java.io.InputStreamReader;
 public class Test_14501 {
     private static int size =0;
     private static int[][] bf;
-    private static int[] tArr;
-    private static int[] pArr;
+    private static int[] duration;
+    private static int[] price;
 
-    public static int getMax(int now, int start) {
-        if(start > size-1) {   // 모든 인덱스 경우를 훑은 후
-            return Math.max(bf[size-1][0],bf[size-1][1]);   // 둘 중 최대값
-        }
+    public static int getMax() {
+        for(int idx=1; idx<=size; idx++) {
+//            System.out.print(bf[idx][0] +", " + bf[idx][1] + " 에서 ~~~~~~~~~~>>>>>");
+            if (idx != size && idx + duration[idx-1] <= size+1) {  // 마지막 Index 전까지의 로직
+                if (bf[idx][1] > bf[idx-1][0]) {
+                    bf[idx][0] = bf[idx][1];
+                    bf[idx][1] += price[idx - 1];
+                } else {
+                    if(duration[idx-1] ==1) {   // 작업기간이 하루라면
+                        bf[idx][1] += price[idx-1];
+                        bf[idx][0] += price[idx-1];
+                    } else {
+                        bf[idx][1] = bf[idx-1][0] + price[idx - 1];
+                    }
+                }
 
-        if(now >= size) {
-            getMax(start+1, start+1);
-        } else {
-            bf[start][0] = bf[start][1];
-            bf[start][1] += pArr[now];
-
-            for(int i=now; i<size; i++) {
-                bf[start][0] = Math.max(bf[start][1], bf[start][0] + pArr[i]);
-                if(now < size-1 ) {
-                    getMax(now + tArr[i], start);
+                if (bf[idx + duration[idx - 1]][1] < price[idx - 1]) { // 작업기간 이후 번 돈 저장
+                    bf[idx + duration[idx - 1]][1] += bf[idx][1];
+                }
+                if (bf[idx + duration[idx - 1]][0] < price[idx - 1]) { // 작업기간 이후 번 돈 저장
+                    bf[idx + duration[idx - 1]][0] += bf[idx][0];
+                }
+            } else {    // 마지막 Index
+                if(duration[idx-1] ==1) {
+                    bf[idx][1] += price[idx-1];
+                    bf[idx][0] += price[idx-1];
                 }
             }
+//            System.out.println(bf[idx][0] + ", " + bf[idx][1] + "로 변함");
         }
 
-        return Math.max(bf[size-1][0],bf[size-1][1]);
+        int max =0;
+        for(int i=1; i<=size; i++) {
+            max = Math.max(Math.max(bf[i][0], bf[i][1]), max);
+        }
+
+        return max;
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
         size = Integer.parseInt(br.readLine());
 
-        tArr = new int[size];
-        pArr = new int[size];
+        duration = new int[size];
+        price = new int[size];
 
         for(int i=0; i<size; i++) {
             String str = br.readLine();
-            tArr[i] = Integer.parseInt(str.split(" ")[0]);
-            pArr[i] = Integer.parseInt(str.split(" ")[1]);
+            duration[i] = Integer.parseInt(str.split(" ")[0]);
+            price[i] = Integer.parseInt(str.split(" ")[1]);
         }
 
-        bf = new int[size][2];
+        bf = new int[size+2][size+2];
 
-        System.out.println(getMax(0,0));
+        System.out.println(getMax());
 
         br.close();
     }
