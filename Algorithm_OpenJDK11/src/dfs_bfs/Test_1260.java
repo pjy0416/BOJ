e59d6060b32bce83ddf8f78eec605f4f5f2745b8
dimpl@JinYoung <dimple0416@naver.com>
2019-02-18 18:58:50 +0900

    A solution of a BOJ Test 1260 without STL (BFS, DFS)

--- Algorithm_OpenJDK11/src/dfs_bfs/Test_1260.java
+++ Algorithm_OpenJDK11/src/dfs_bfs/Test_1260.java
@@ -3,8 +3,6 @@ package dfs_bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Test_1260 {
     public static void main(String[] args) throws IOException {
@@ -56,7 +54,7 @@ class Graph {
 
     private void dfs(int start, boolean[] isVisited) {            // DFS 탐색
         System.out.print(start + " ");
-        
+
         isVisited[start] = true;                                  // 방문기록 작성
         for(int i=1; i<=nodeNum; i++) {                           // loop 돌면서
             if(adj[start][i] ==1) {                               // 현재 들어와있는 Node에 간선 연결된 곳이 있을 때
@@ -68,6 +66,32 @@ class Graph {
     }
 
     private void bfs(int start, boolean[] isVisited) {                                           // BFS 탐색
+        int[] queue = new int[nodeNum+1];                                                       // Queue Arr 생성
+        int size =0;                 // queue의 실질적 사이즈
+        int top =0;                 // 현재위치
+
+        queue[size++] = start;      // 사이즈 증가하며 추가
+        isVisited[start] = true;    // 현재 노드 방문
+
+        while(true) {
+            int num = queue[top++]; // 현재 위치인 top을 불러오면서 다음 idx로 이동
+
+            if(num == 0) {          // queue 크기를 초과했다는 의미 이기 때문에
+                break;              // 멈춰준다.
+            }
+
+            System.out.print(num + " ");        // queue에 노드가 남아있으면 print
+            for(int i=1; i<=nodeNum; i++) {     // looping
+                if(adj[num][i] == 1) {          // 현재 노드에서 연결된 간선이 있을 때
+                    if(!isVisited[i]) {         // 탐색한 적이 없다면
+                        queue[size++] = i;      // queue 사이즈를 증가시키며
+                        isVisited[i] = true;    // 들렸다는 표시를 해줌
+                    }
+                }
+            }
+        }
+
+        /*
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
         isVisited[start] = true;
@@ -84,9 +108,10 @@ class Graph {
                 }
             }
         }
+        */
     }
 
-    private boolean[] initBool() {
+    private boolean[] initBool() {      // isVisited 배열을 생성, 반환
         boolean[] isVisited = new boolean[nodeNum+1];
 
         for(int i=1; i<=nodeNum; i++) {
