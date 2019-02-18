259fd9f400147183cd41bd7357bae0b7a66b503f
dimpl@JinYoung <dimple0416@naver.com>
2019-02-18 18:25:15 +0900

    (Wrong Code) Test1260 (BFS,DFS)

--- Algorithm_OpenJDK11/src/dfs_bfs/Test_1260.java
+++ Algorithm_OpenJDK11/src/dfs_bfs/Test_1260.java
@@ -3,10 +3,9 @@ package dfs_bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 
 public class Test_1260 {
-    public static void main(String[] args) throws IOException { //
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String infoStr = br.readLine();
 
@@ -28,95 +27,79 @@ public class Test_1260 {
 
 class Graph {
     private int nodeNum;                    // 노드의 갯수
-    private LinkedList<Integer>[] adj;      // 인접리스트 Adjacency List
-    private Boolean[] isVisited;            // 방문 기록 Array
+    private int[][] adj;
 
     //Constructor
     Graph(int num) {
         nodeNum = num;
-        adj = new LinkedList[num+1];        // index의 시작이 0이 아닌 1부터 시작하기 위해서
-        isVisited = new Boolean[num+1];     // 같은 이유
-
-        for(int i=1; i<=num; i++) {         // 초기화
-            adj[i] = new LinkedList<Integer>();
-            isVisited[i] = false;
-        }
+        adj = new int[num+1][num+1];        // index의 시작이 0이 아닌 1부터 시작하기 위해서
     }
 
     void add(int major, int target) {   // 양방향으로 간선 이어주기
-        adj[major].add(target);
-        adj[target].add(major);
+        adj[major][target] =1;
+        adj[target][major] =1;
     }
 
     void printDFS(int start) {          // DFS 실행 결과 print
-        LinkedList<Integer>[] adjCopy = adjClone();
-        dfs(start, adjCopy);
+        boolean[] isVisited = initBool();
+
+        dfs(start, isVisited);
     }
 
     void printBFS(int start) {          // BFS 실행 결과 print
         System.out.println();       // DFS결과와 띄워주기 위한 프린트
-        bfs(start);
+        boolean[] isVisited = initBool();
+        bfs(start, isVisited);
     }
 
-    private void dfs(int start, LinkedList<Integer>[] adjCopy) {            // DFS 탐색
-        if(!adjCopy[start].isEmpty()) { // 리스트에 연결되어 있는 것이 있으면
-            int num = adjCopy[start].pop();     //pop으로 꺼내주고
-
-            if(!isVisited[start]) {             // 들린적이 없으면
-                System.out.print(start + " ");  // print
+    private void dfs(int start, boolean[] isVisited) {            // DFS 탐색
+        if(!isVisited[start]) {
+            System.out.print(start + " ");
+        }
+        isVisited[start] = true;
+        for(int i=1; i<=nodeNum; i++) {
+            if(adj[start][i] ==1) {
+                if(!isVisited[i]) {
+                    dfs(i,isVisited);
+                }
             }
-
-            if(isVisited[num]) {                // 이미 방문했었으면
-                num = adjCopy[start].pop();     // 다시 pop~~
-                dfs(num, adjCopy);
-            }
-
-            isVisited[start] = true;            // 들렸다는 표시
-            dfs(num, adjCopy);                  // 꺼낸 index로 들어감
         }
     }
 
-    private void bfs(int start) {                                           // BFS 탐색
-        for(int i=1; i<=nodeNum; i++) { //  방문기록 초기화
-            isVisited[i] = false;
-        }
-
-        int[] queue = new int[1001];    // queue 생성
-        int top =0;                     // 위치한 곳을 표시할 top
+    private void bfs(int start, boolean[] isVisited) {                                           // BFS 탐색
+        int[] queue = new int[nodeNum+1];    // queue 생성
         int size =0;                    // 전체 Size
 
         System.out.print(start + " ");
         isVisited[start] = true;
-        while(!adj[start].isEmpty()) {
-            int num = adj[start].pop();
-            queue[size++] = num;
 
-            if(!isVisited[num]) {
-                System.out.print(num + " ");
-            }
-
-            isVisited[num] =true;
-
-            if(adj[start].isEmpty()) {
-                start = queue[top++];
-            }
-
-            if(top >= nodeNum) {
-                break;
+        for(int i=1; i <=nodeNum; i++) {
+            if(adj[start][i] ==1) {
+                queue[size++] = i;
+                System.out.print(i + " ");
+                isVisited[i] = true;
             }
         }
 
+        for(int i=0; i<size; i++) {
+            for(int idx=1; idx<=nodeNum; idx++) {
+                if(adj[queue[i]][idx] ==1) {
+                    if(!isVisited[idx]) {
+                        System.out.print(idx + " ");
+                        isVisited[idx] = true;
+                    }
+                }
+            }
+        }
     }
 
-    LinkedList<Integer>[] adjClone() {      // 인접 리스트 복사 메소드
-        LinkedList<Integer>[] result= new LinkedList[nodeNum+1];
+    private boolean[] initBool() {
+        boolean[] isVisited = new boolean[nodeNum+1];
 
         for(int i=1; i<=nodeNum; i++) {
-            result[i] = new LinkedList<Integer>();
-            result[i] = (LinkedList<Integer>) adj[i].clone();
+            isVisited[i] = false;
         }
 
-        return result;
+        return isVisited;
     }
-
 }
