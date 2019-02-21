5e26801dc04ffb36cd3e9c25934544045dda566e
dimpl@JinYoung <dimple0416@naver.com>
2019-02-21 23:13:48 +0900

    Test 2178 code arrangement(BFS : 미로 탐색)

--- Algorithm_OpenJDK11/src/dfs_bfs/Test_2178.java
+++ Algorithm_OpenJDK11/src/dfs_bfs/Test_2178.java
@@ -34,31 +34,31 @@ public class Test_2178 {
             int addSize =0;             // 큐에 몇개가 들어갔는지 판단하기 위한 변수
 //            System.out.println(" ");
 //            System.out.println(first + " \t" + size);
-            for(int idx=first; idx<size; idx++) {   
+            for(int idx=first; idx<size; idx++) {   // queue에서 꺼내지 않은 index부터 size까지 꺼냄. <- 모든 경우 탐색
                 int nx = queueX[idx];
                 int ny = queueY[idx];
 //                System.out.println(idx+"번 째 \t" + ny + "," + nx + "가 큐에 있음");
-                for (int i = 0; i < 4; i++) {
-                    int y = ny + wayY[i];
-                    int x = nx + wayX[i];
+                for (int i = 0; i < 4; i++) {   // 동서남북 검증
+                    int y = ny + wayY[i];   // 동서남북에 맞게
+                    int x = nx + wayX[i];   // 좌표를 증가 or 감소
 
 //                    System.out.println(ny +"," +nx +" 꺼내서 씀\t" + y + "," +x + "로 변환");
 
                     if (y <= n - 1 && y >= 0 && x >= 0 && x <= m - 1) {             // out of index 방지
                         if (!isVisited[y][x] && map[y].charAt(x) == '1') {          // 방문한 적이 없고 길이 있으면 go!
 //                            System.out.println(y + "," + x + "가자");
-                            queueX[size+addSize] = x;
+                            queueX[size+addSize] = x;       //  queue 에 좌표를 저장
                             queueY[size+addSize] = y;
-                            addSize++;
-                            isVisited[y][x] = true;
+                            addSize++;                      // queue 에 넣을때마다 증가
+                            isVisited[y][x] = true;         // 방문기록 남김
                         }
                     }
                 }
-                first++;
+                first++;                                    // 마지막으로 꺼낸 index 값 증가
             }
 
-            size += addSize;
-            cnt++;
+            size += addSize;                                // queue 전체 사이즈를 넣은 갯수만큼 증가
+            cnt++;                                          // step count
         }
     }
 
