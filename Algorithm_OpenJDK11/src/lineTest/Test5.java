749c41a835c5eadf8255ec08f17a0a1c9d4f0f1c
dimpl@JinYoung <dimple0416@naver.com>
2020-04-05 23:16:05 +0900

    modify : modify a sorting method(Line Test 2020,JAVA)

--- Algorithm_OpenJDK11/src/lineTest/Test5.java
+++ Algorithm_OpenJDK11/src/lineTest/Test5.java
@@ -52,6 +52,9 @@ public class Test5 {
                 {"doc2", "t0", "t2", "t3"},
                 {"doc3", "t1", "t6", "t7"},
                 {"doc4", "t1", "t2", "t4"},
+                {"dOc4", "t1", "t2", "t4"},
+                {"doc44", "t1", "t2", "t4"},
+                {"doca44", "t1", "t2", "t4"},
                 {"doc5", "t6", "t100", "t8"}
         };
         String[] tags = {"t1", "t2", "t3"};
@@ -103,13 +106,43 @@ class Documents implements Comparable<Documents>{
         if(this.score < o.score) {
             return 1;
         } else if(this.score == o.score) {
-            // 여기서 사전식으로 정렬 반환
-            return this.docName.compareTo(o.getName()) > 0 ? 1 : -1;
+            String originStr = this.docName.replaceAll("[0-9]","");
+            String targetStr = o.getName().replaceAll("[0-9]", "");
+            String originNum = this.docName.replaceAll("[^0-9]","");
+            String targetNum = o.getName().replaceAll("[^0-9]", "");
+            for(int i=0; i<originStr.length(); i++) {
+                for(int j=0; j<targetStr.length(); j++) {
+                    if(i == j) {
+                        char originCh = originStr.charAt(i);
+                        char targetCh = targetStr.charAt(j);
+                        int originCategory = charOrder(originCh);
+                        int targetCategory = charOrder(targetCh);
+
+                        if(originCategory == targetCategory) {
+                            if(originCh > targetCh) {
+                                return 1;
+                            } else if(originCh < targetCh) {
+                                return -1;
+                            }
+                            // 같은 경우는 스킵
+                        } else {    // 다른 경우는 바로 리턴
+                            return originCategory > targetCategory ? 1 : -1;
+                        }
+                    }
+                }
+            }
+
+            //위에서 결론이 안났다면 비교한 곳까지는 똑같다는 의미
+            if(originStr.length() == targetStr.length()) {  // 길이가 같은 아이라면 뒤에 숫자 비교
+                return Integer.parseInt(originNum) > Integer.parseInt(targetNum) ? 1 : -1;
+            } else {
+                return originStr.length() > targetStr.length() ? 1: -1;
+            }
         }
         return -1;
     }
 
-    /*private int charOrder(char ch) {
+    private int charOrder(char ch) {
         if(ch >=97 && ch <= 122) {   // 소문자
             return 1;
         } else if(ch >=65 && ch <= 90) {
@@ -119,5 +152,5 @@ class Documents implements Comparable<Documents>{
         } else {
             return 4;
         }
-    }*/
+    }
 }
