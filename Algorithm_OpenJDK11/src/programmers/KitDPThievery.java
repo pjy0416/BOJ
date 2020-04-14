37af0cbd46abd00832283dea99ca81cb9322b485
dimpl@JinYoung <dimple0416@naver.com>
2020-04-14 22:52:47 +0900

    modify : 1 Array ~> 2 Array (HighScoreKit_DP, JAVA)

--- Algorithm_OpenJDK11/src/programmers/KitDPThievery.java
+++ Algorithm_OpenJDK11/src/programmers/KitDPThievery.java
@@ -4,29 +4,18 @@ public class KitDPThievery {
     private static int solution(int[] money) {
         //이 마을에 있는 집은 3개 이상 1,000,000개 이하입니다.
         int len = money.length;
-        int[] answer = new int[len+1];
+        int[] dp1 = new int[len], dp2 = new int[len];
 
-        answer[0] = money[len-1];
-        answer[1] = money[0];
-        answer[2] = money[1] + answer[0];
+        dp1[0] = money[0];  // 첫번째 돈 포함
+        dp1[1] = money[0];  // 두번째 돈은 포함을 안할거니까 첫번째 애 입력
+        dp2[1] = money[1];  // 두번째 돈 포함
 
-        for (int i = 3; i <= len; i++) {
-            answer[i] = answer[i-2] + money[i-1] > answer[i-1] ? answer[i-2] + money[i-1] : answer[i-1];
+        for (int i = 2; i <len; i++) {  // 돈은 하나 당겨서 생각
+            dp1[i] = Math.max(dp1[i-2]+money[i], dp1[i-1]);
+            dp2[i] = Math.max(dp2[i-2]+money[i], dp2[i-1]);
         }
 
-/*
-        for(int num : answer) {
-            System.out.println(num);
-        }
-*/
-
-        /*if(money[0] < money[money.length-1]) {
-            answer[money.length-1] -= money[0];
-        } else {
-            answer[money.length-1] -= money[money.length-1];
-        }
-*/
-        return answer[money.length-1];
+        return Math.max(dp1[len-2], dp2[len-1]);    // dp1에는 첫번째 집이 포함됐으니까 최대 길이 하나 전 IDX를 return
     }
 
     public static void main(String[] args) {
