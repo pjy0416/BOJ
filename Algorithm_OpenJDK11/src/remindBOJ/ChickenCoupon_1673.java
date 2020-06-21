ba4aff3bf2f80a8f82cd9484405b6c904f3763f1
dimpl@JinYoung <dimple0416@naver.com>
2020-06-22 01:13:40 +0900

    A solution of BOJ Chicken coupon, 1673(JAVA)

---
+++ Algorithm_OpenJDK11/src/remindBOJ/ChickenCoupon_1673.java
@@ -0,0 +1,33 @@
+package remindBOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ChickenCoupon_1673 {
+    public static void main(String[] args) throws IOException {
+        solution(new BufferedReader(new InputStreamReader(System.in)));
+    }
+
+    private static void solution(BufferedReader br) throws IOException {
+        String inputStr = "";
+        while((inputStr = br.readLine()) != null) {
+            System.out.println(getChickens(new StringTokenizer(inputStr)));
+        }
+
+        br.close();
+    }
+
+    private static long getChickens(StringTokenizer st) {
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int result =n;
+        while(n/k !=0) {
+            result += n/k;
+            n = n/k + n%k;
+        }
+        return result;
+    }
+}
