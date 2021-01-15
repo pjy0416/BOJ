9408376ea9a854376967a126d82fdf6f4d12661e
dimpl@JinYoung <dimple0416@naver.com>
2021-01-15 15:27:03 +0900

    백준 돌 게임 9655번 문제 풀이

---
+++ Algorithm_OpenJDK11/src/remindBOJ/StoneGame_9655.java
@@ -0,0 +1,18 @@
+package remindBOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StoneGame_9655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n %2 ==0) {
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+        br.close();
+    }
+}
