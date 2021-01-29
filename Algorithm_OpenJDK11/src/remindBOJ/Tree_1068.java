3cdf5148d736a20b7a70855bc8585a9d2a3bd82d
dimpl@JinYoung <dimple0416@naver.com>
2021-01-29 12:05:46 +0900

    백준 트리 1068번 문제 풀이

---
+++ Algorithm_OpenJDK11/src/remindBOJ/Tree_1068.java
@@ -0,0 +1,68 @@
+package remindBOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Tree_1068 {
+    static final int ROOT =-1, REMOVED =-2, ZERO =0, ADDITIONAL =1;
+    static int[] parents;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        parents = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            parents[i] = Integer.parseInt(st.nextToken());
+        }
+        int removeNode = Integer.parseInt(br.readLine());
+
+        br.close();
+        solution(removeNode, n);
+    }
+
+    private static void solution(int removeNode, int n) {
+        parents[removeNode] = REMOVED;
+        for(int i=0; i<n; i++) {
+            if(find(parents[i]) == removeNode) { // 제거해야함
+                union(i, removeNode);
+            }
+        }
+        int answer =0;
+        for(int i=0; i<n; i++) {
+            if(parents[i] != REMOVED) { // 자신이 루트가 아니고 지워지지 않은 경우
+                answer += find(parents[i]) != REMOVED && !hasChildren(i) ? ADDITIONAL : ZERO;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static boolean hasChildren(int i) {
+        for(int parentIdx : parents) {
+            if(parentIdx == i) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int find(int val) {
+        if(val == REMOVED) {
+            return val;
+        } else if(val == ROOT) {
+            return val;
+        }
+        return find(parents[val]);
+    }
+
+    private static void union(int left, int right) {
+        left = find(left);
+        right = find(right);
+        if(left != right) {
+            parents[left] = right;
+        }
+    }
+}
